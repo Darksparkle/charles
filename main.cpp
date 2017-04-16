3ca67b9206aeab4bd594a9a64aa4e3a3ea3aaaeb
kodikae <ilosaske@gmail.com>
2017-04-16 14:12:27 +0200

    comments added

--- main.cpp
+++ main.cpp
@@ -11,24 +11,26 @@ std::string winname = "Window";
 
 int main(int argc, char** argv) {
 
-  cv::namedWindow(winname);
-  cv::VideoCapture cap;
-  cv::Mat frame;
+  cv::namedWindow(winname); // main window
+  cv::VideoCapture cap; // webcam capture device
+  cv::Mat frame; // image frame
 
   if(!cap.open(0)) {
+    // no webcam found
     printf("[!] Error: couldn't load camera");
     return 0;
   }
 
   for (;;) {
-    cap >> frame;
-    if(frame.empty()) break;
-    cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-    imshow(winname, frame);
-    if(cv::waitKey(10) == 27) break;
-    if(cv::getWindowProperty(winname, 0) < 0) break;
+    cap >> frame; // read from webcam into frame
+    if(frame.empty()) break; // no data found in frame
+    cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY); // convert frame to greyscale
+    imshow(winname, frame); // show frame in window
+    if(cv::waitKey(10) == 27) break; // wait for escape key
+    if(cv::getWindowProperty(winname, 0) < 0) break; // break if window is closed
   }
 
+  // release memory
   cap.release();
   cv::destroyWindow(winname);
   frame.deallocate();
