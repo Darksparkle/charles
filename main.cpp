e812b9beea9ef0b83778c6cc7d610dce84cc1f86
kodikae <ilosaske@gmail.com>
2017-04-16 15:23:43 +0200

    chess

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main(int argc, char** argv) {
   cv::namedWindow(winname); // main window
   cv::VideoCapture cap; // webcam capture device
   cv::Mat frame; // image frame
+  cv::Mat gray;
   std::vector<cv::Point2f> corners;
   cv::Size patternsize(8,6);
 
@@ -27,13 +28,14 @@ int main(int argc, char** argv) {
   for (;;) {
     cap >> frame; // read from webcam into frame
     if(frame.empty()) break; // no data found in frame
-    cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY); // convert frame to greyscale
-    imshow(winname, frame); // show frame in window
+    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY); // convert frame to greyscale
     if(cv::waitKey(10) == 27) break; // wait for escape key
     if(cv::getWindowProperty(winname, 0) < 0) break; // break if window is closed
-    bool patternFound = cv::findChessboardCorners(frame, patternsize, corners);
-    if(patternFound) cv::cornerSubPix(frame, corners, cv::Size(11,11), cv::Size(-1,-1), cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+    bool patternFound = cv::findChessboardCorners(gray, patternsize, corners, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
+    if(patternFound) cv::cornerSubPix(gray, corners, cv::Size(11,11), cv::Size(-1,-1), cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
     cv::drawChessboardCorners(frame, patternsize, cv::Mat(corners), patternFound);
+    imshow(winname, frame); // show frame in window
+    
   }
 
   // release memory
