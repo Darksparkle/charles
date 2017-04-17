68f39a80fdd368256e43586d5ca34dbb8d1e93ab
kodikae <ilosaske@gmail.com>
2017-04-17 22:51:10 +0200

    should maybe work

--- main.cpp
+++ main.cpp
@@ -24,10 +24,28 @@ std::ofstream file;
 const int win_width = 800;
 const int win_height = 640;
 
+void drawQuad(cv::Mat image, cv::Mat points, cv::Scalar color) {
+  std::cout << points.at<cv::Point2f>(0,0) << " " << points.at<cv::Point2f>(0,2) << " " << points.at<cv::Point2f>(0,3) << '\n';
+  cv::line(image, points.at<cv::Point2f>(0,0), points.at<cv::Point2f>(0,1), color);
+  cv::line(image, points.at<cv::Point2f>(0,1), points.at<cv::Point2f>(0,2), color);
+  cv::line(image, points.at<cv::Point2f>(0,2), points.at<cv::Point2f>(0,3), color);
+  cv::line(image, points.at<cv::Point2f>(0,3), points.at<cv::Point2f>(0,0), color);
+}
+
 int main(int argc, char** argv) {
 
   cv::namedWindow(winname, cv::WINDOW_OPENGL);
   cv::resizeWindow(winname, win_width, win_height);
+
+  cv::FileStorage fs("data.xml", cv::FileStorage::READ);
+  cv::Mat intrinsics, distortion;
+  fs["Camera_Matrix"] >> intrinsics;
+  fs["Distortion_Coefficients"] >> distortion;
+
+  if (intrinsics.rows != 3 || intrinsics.cols != 3 || distortion.rows != 5 || distortion.cols != 1) {
+    std::cout << "Run calibration (in ../calibrate/) first!" << '\n';
+    return 1;
+  }
 
   cv::VideoCapture cap; // webcam capture device
 
@@ -36,20 +54,9 @@ int main(int argc, char** argv) {
   cv::Mat thresh;
   cv::Mat blur;
 
-  std::vector<cv::Point2f> corners;
-  cv::Size patternsize;
-  if(argc > 1) {
-    patternsize = {atoi(argv[1]), atoi(argv[2])};
-  } else {
-    patternsize = {6,6};
-  }
-
-  cv::Mat intrinsics, distortion;
-  std::vector<cv::Point3f> boardPoints;
-
   file.open("points");
 
-  if(!cap.open(0)) {
+  if (!cap.open(0)) {
     // no webcam found
     printf("[!] Error: couldn't load camera");
     return 0;
@@ -57,25 +64,52 @@ int main(int argc, char** argv) {
 
   for (;;) {
     cap >> frame; // read from webcam into frame
-    if(frame.empty()) break; // break on no data found in frame
+    if (frame.empty()) break; // break on no data found in frame
+
     cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY); // convert frame to greyscale
     cv::blur(gray, blur, cv::Size(5,5));
     cv::threshold(blur, thresh, 128.0, 255.0, cv::THRESH_OTSU);
+
     std::vector<std::vector<cv::Point> > contours;
     cv::findContours(thresh, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+
     cv::Scalar color(0,255,0);
-    std::vector<std::vector<cv::Point> > squares;
+
+    std::vector<cv::Mat> squares;
     for(auto contour : contours) {
       std::vector<cv::Point> approx;
       cv::approxPolyDP(contour, approx, cv::arcLength(cv::Mat(contour), true)*0.02, true);
       if(approx.size() == 4 && fabs(cv::contourArea(cv::Mat(approx))) > 1000 && cv::isContourConvex(cv::Mat(approx))) {
-        squares.push_back(approx);
+        cv::Mat squareMat;
+        cv::Mat(approx).convertTo(squareMat, CV_32FC3);
+        squares.push_back(squareMat);
       }
     }
-    cv::drawContours(frame, squares, -1, color);
+
+    if (squares.size() > 0) {
+      std::vector<cv::Point3f> objectPoints = {cv::Point3f(-1, -1, 0), cv::Point3f(-1, 1, 0), cv::Point3f(1, 1, 0), cv::Point3f(1, -1, 0)};
+      cv::Mat objectPointsMat(objectPoints);
+
+      std::cout << "objectPointsMat: " << objectPointsMat.rows << ", " << objectPointsMat.cols << '\n';
+      std::cout << "squares[0]: " << squares[0] << '\n';
+
+      cv::Mat rvec, tvec;
+      cv::solvePnP(objectPointsMat, squares[0], intrinsics, distortion, rvec, tvec);
+
+      std::cout << "rvec: " << rvec << '\n' << "tvec: " << tvec << '\n';
+
+      drawQuad(frame, squares[0], color);
+    }
+
+    // cv::drawContours(frame, squares, -1, color);
+
     imshow(winname, frame);
-    if(cv::waitKey(10) == 27) break; // break on escape key pressed
-    if(cv::getWindowProperty(winname, 0) < 0) break; // break if window is closed
+
+    if (cv::waitKey(10) == 27)
+      break; // break on escape key pressed
+
+    if (cv::getWindowProperty(winname, 0) < 0)
+      break; // break if window is closed
   }
 
   // release memory
